a2c1e1bab71528f4d6b7d85fb7a4c1d435c99322
sumitanand <sumit.anandd786@gmail.com>
2023-03-03 16:42:52 +0530

    1. Camerax implementation 2. ML Kit with vision api implemented

--- app/src/main/java/com/facedetectiondemo/SplashActivity.kt
+++ app/src/main/java/com/facedetectiondemo/SplashActivity.kt
@@ -1,12 +1,45 @@
 package com.facedetectiondemo
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity:AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        initData()
     }
+
+    fun initData() {
+        pbSplash.visibility = View.VISIBLE
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        //Normal Handler is deprecated , so we have to change the code little bit
+
+        // Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
+            pbSplash.visibility = View.GONE
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
+
 }
